b4d0474404151aa7bf283de9d67f98244baffd16
Dan <danf425@gmail.com>
2019-08-12 19:18:04 +0000

    conteso

---
+++ habitat/plan.ps1
@@ -0,0 +1,32 @@
+# This file is the heart of your application's habitat.
+# See full docs at https://www.habitat.sh/docs/reference/plan-syntax/
+
+# Required.
+# Sets the name of the package. This will be used along with `$pkg_origin`,
+# and `$pkg_version` to define the fully-qualified package name, which determines
+# where the package is installed to on disk, how it is referred to in package
+# metadata, and so on.
+$pkg_name="contosouniversity"
+$pkg_origin="danf"
+$pkg_version="1.2.0"
+
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+
+$pkg_license=@("Apache-2.0")
+
+$pkg_deps=@("core/dsc-core")
+$pkg_build_deps=@("core/nuget")
+function Invoke-Build {
+  Copy-Item $PLAN_CONTEXT/../* $HAB_CACHE_SRC_PATH/$pkg_dirname -recurse -force
+  nuget restore "$HAB_CACHE_SRC_PATH/$pkg_dirname/C#/$pkg_name/packages.config" -PackagesDirectory "$HAB_CACHE_SRC_PATH/$pkg_dirname/C#/packages" -Source "https://www.nuget.org/api/v2"
+  nuget install MSBuild.Microsoft.VisualStudio.Web.targets -Version 14.0.0.3 -OutputDirectory $HAB_CACHE_SRC_PATH/$pkg_dirname/
+  $env:VSToolsPath = "$HAB_CACHE_SRC_PATH/$pkg_dirname/MSBuild.Microsoft.VisualStudio.Web.targets.14.0.0.3/tools/VSToolsPath"
+  ."$env:SystemRoot\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "$HAB_CACHE_SRC_PATH/$pkg_dirname/C#/$pkg_name/${pkg_name}.csproj" /t:Build /p:VisualStudioVersion=14.0
+  if($LASTEXITCODE -ne 0) {
+      Write-Error "dotnet build failed!"
+  }
+}
+
+function Invoke-Install {
+  ."$env:SystemRoot\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "$HAB_CACHE_SRC_PATH/$pkg_dirname/C#/$pkg_name/${pkg_name}.csproj" /t:WebPublish /p:WebPublishMethod=FileSystem /p:publishUrl=$pkg_prefix/www
+}
---
+++ results/pre_build.ps1
@@ -0,0 +1,5 @@
+$pkg_origin="danf"
+$pkg_name="contosouniversity"
+$pkg_version="1.2.0"
+$pkg_release="20190812191556"
+$pkg_ident="danf/contosouniversity/1.2.0/20190812191556"
